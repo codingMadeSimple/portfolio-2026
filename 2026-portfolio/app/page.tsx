ea23aafff91a0163f1e83164923f2bd2e4993d38
Colin Spangelo <colin.spangelo@gmail.com>
2026-05-08 16:23:25 -0600

    added faq, cta, logos, and footer components.

--- 2026-portfolio/app/page.tsx
+++ 2026-portfolio/app/page.tsx
@@ -1,11 +1,23 @@
 import Navbar from "@/components/sections/navbar/default"
 import { ModeToggle } from "@/hooks/mode-toggle"
+import Hero from "@/components/sections/hero/default"
+import Logos from "@/components/sections/logos/default"
+import Items from "@/components/sections/items/default"
+import FAQ from "@/components/sections/faq/default"
+import CTA from "@/components/sections/cta/default"
+import { Footer } from "@/components/ui/footer"
 
 export default function Page() {
   return (
     <div>
       <Navbar/>
-      <ModeToggle/>
+      <Hero/>
+      <Logos/>
+      <Items/>
+      <FAQ/>
+      <CTA/>
+      <Footer/>
+      {/* <ModeToggle/> */}
     </div>
     )
 }
